cd0a00ac2c3069e606ef099556254e03a8443451
Carryan <chenlvan@foxmail.com>
2018-12-20 17:19:44 +0800

    修改配置文件

--- js/storageKeyName.js
+++ js/storageKeyName.js
@@ -2,10 +2,10 @@
 
 var storageKeyName = (function(mod) {
 
-	mod.key = 1;//0开发，1部署
+	mod.key = 0;//0测试；1正式
 	var exLog = console.log;
 	console.log = function(hint, object) {
-		if(mod.key === 1) {
+		if(mod.key === 0) {
 			var argus = hint;
 			if(typeof(object) !== 'undefined') {
 				argus = hint + JSON.stringify(object);
@@ -14,81 +14,70 @@ var storageKeyName = (function(mod) {
 		}
 	}
 	switch(mod.key) {
-		case 0: //开发
-			mod.SCHOOLID = 100005; //学校ID
-			mod.USERTYPE = 0; //用户类型，0老师,1家长,2学生
-			mod.INTERFACEGU = 'https://jsypay.jiaobaowang.net/useradminwebapi/api/data/'; //顾工接口
-			mod.INTERFACEKONG = 'https://jbyj.jiaobaowang.net/SchoolOAService/'; //孔工接口
-			mod.INTERFACESIGNINKONG = 'https://jbyj.jiaobaowang.net/SchoolAttendanceService/'; //孔工接口IMG
-			mod.TEACHERIMG = 'http://jsypay.jiaobaowang.net/jsyadmin/upuserimg.ashx?userid='; //老师上传头像
-			mod.ANDROIDUPDATEURL='http://192.168.1.121:8081/app/versionCode.xml';
-
-			//---七牛空间和接口---
-			mod.QNPB = 'https://qn-educds.jiaobaowang.net/'; //公开空间域名
-			mod.QNGETUPLOADTOKEN = 'https://jbyc.jiaobaowang.net:8504/Api/QiNiu/GetUpLoadToKen';
-			mod.QNGETUPTOKENHEADIMGE = 'https://jbyc.jiaobaowang.net:8504/Api/QiNiu/GetUpLoadToKen'; //获取上传个人头像，群头像，资料头像到七牛的token的url
-			mod.QNGETUPTOKENFILE = 'https://jbyc.jiaobaowang.net:8504/Api/QiNiu/GetUpLoadToKen'; //获取上传文件（云存储）到七牛的token的url
-			mod.QNGETDOWNTOKENFILE = 'http://114.215.222.186:8004/Api/QiNiu/GetAccess'; //获取下载文件（云存储）的token的url，url+七牛文件url
-			mod.QNGETTOKENDELETE = 'http://114.215.222.186:8004/Api/QiNiu/Delete'; //获取批量（或者一个）删除七牛文件的token的url
-			break;
-		case 1: //发布
+		case 0: //测试
 			mod.SCHOOLID = 100008; //学校ID
-			mod.USERTYPE = 0; //用户类型，0老师,1家长,2学生
-			mod.INTERFACEGU = 'https://zhxy.jiaobaowang.net:8515/schadminwebapi/api/data/'; //顾工接口
-			mod.INTERFACEKONG = 'https://jbyj.jiaobaowang.net:8443/SchoolOAService/'; //孔工接口
-			mod.INTERFACESIGNINKONG = 'https://jbyj.jiaobaowang.net:8443/SchoolAttendanceService/'; //孔工接口
-			mod.TEACHERIMG = 'https://zhxy.jiaobaowang.net:8515/schadminwebadmin/upuserimg.ashx?userid='; //老师上传头像
-			mod.ANDROIDUPDATEURL='http://zhxy.jiaobaowang.net:8015/appupdate/versionCode.xml';
+			mod.USERTYPE = 2; //用户类型，0老师,1家长,2学生
+			mod.INTERFACEGU = 'https://zhxy.jiaobaowang.net:8515/schadminwebapi/api/data/'; //用户信息接口
+//			mod.TEACHERIMG = 'https://zhxy.jiaobaowang.net:8515/schadminwebadmin/upuserimg.ashx?userid='; //老师上传头像
+//			mod.ANDROIDUPDATEURL='http://zhxy.jiaobaowang.net:8015/appupdate/versionCode.xml';
 
 			//---七牛空间和接口---
-			mod.QNPB = 'https://qn-educds.jiaobaowang.net/'; //公开空间域名
-			mod.QNGETUPLOADTOKEN = 'https://jbyc.jiaobaowang.net:8504/Api/QiNiu/GetUpLoadToKen';
-			mod.QNGETUPTOKENHEADIMGE = 'https://jbyc.jiaobaowang.net:8504/Api/QiNiu/GetUpLoadToKen'; //获取上传个人头像，群头像，资料头像到七牛的token的url
-			mod.QNGETUPTOKENFILE = 'https://jbyc.jiaobaowang.net:8504/Api/QiNiu/GetUpLoadToKen'; //获取上传文件（云存储）到七牛的token的url
-			mod.QNGETDOWNTOKENFILE = 'http://114.215.222.186:8004/Api/QiNiu/GetAccess'; //获取下载文件（云存储）的token的url，url+七牛文件url
-			mod.QNGETTOKENDELETE = 'http://114.215.222.186:8004/Api/QiNiu/Delete'; //获取批量（或者一个）删除七牛文件的token的url
+//			mod.QNPB = 'https://qn-educds.jiaobaowang.net/'; //公开空间域名
+//			mod.QNGETUPLOADTOKEN = 'https://jbyc.jiaobaowang.net:8504/Api/QiNiu/GetUpLoadToKen';
+//			mod.QNGETUPTOKENHEADIMGE = 'https://jbyc.jiaobaowang.net:8504/Api/QiNiu/GetUpLoadToKen'; //获取上传个人头像，群头像，资料头像到七牛的token的url
+//			mod.QNGETUPTOKENFILE = 'https://jbyc.jiaobaowang.net:8504/Api/QiNiu/GetUpLoadToKen'; //获取上传文件（云存储）到七牛的token的url
+//			mod.QNGETDOWNTOKENFILE = 'http://114.215.222.186:8004/Api/QiNiu/GetAccess'; //获取下载文件（云存储）的token的url，url+七牛文件url
+//			mod.QNGETTOKENDELETE = 'http://114.215.222.186:8004/Api/QiNiu/Delete'; //获取批量（或者一个）删除七牛文件的token的url
+			
+			//口语测评接口服务端地址
+			//可用的地址：https://res.jiaobaowang.net; http://139.129.252.49:8080/res; http://192.168.0.122:801/res; http://139.129.252.49:8080/speeking
+			mod.ORALSHOST = "http://139.129.252.49:8080/speeking";
+			
 			break;
+			
+			case 1:
+				mod.SCHOOLID = 100008; //学校ID
+				mod.USERTYPE = 2; //用户类型，0老师,1家长,2学生
+				mod.INTERFACEGU = "";//用户信息接口
+				mod.ORALSHOST = "";//口语测评接口
+			break;
+			
 		default:
 			break;
 	}
 	//七牛上传空间key值
 	//资源平台
-	mod.QNPUBZYKEY = "jsy8004";
+//	mod.QNPUBZYKEY = "jsy8004";
 	//教宝云作业
-	mod.QNPUBJBYZYKEY = "zy309309!";
+//	mod.QNPUBJBYZYKEY = "zy309309!";
 	//教宝云盘
-	mod.QNPUBJBYPKEY = "jbyp@2017";
+//	mod.QNPUBJBYPKEY = "jbyp@2017";
 	//教宝云用户管理
-	mod.QUPUBJBMANKEY = "jbman456";
+//	mod.QUPUBJBMANKEY = "jbman456";
 	//家校圈
-	mod.QNPUBJXQKEY = "jxq789!@";
+//	mod.QNPUBJXQKEY = "jxq789!@";
 	//求知
-	mod.QNPUBQZKEY = "qz123qwe";
+//	mod.QNPUBQZKEY = "qz123qwe";
 	//校讯通
-	mod.QNPUBXXT = "jsy@180526";
+//	mod.QNPUBXXT = "jsy@180526";
 	//七牛存储子空间（文件二级文件名）
-	mod.QNPUBSPACE = "pb"; //七牛公开空间
-	mod.QNPRISPACE = "pv"; //七牛私有空间
-	mod.XXTNOTICE = 'notice/'; //笔校讯通、通知
+//	mod.QNPUBSPACE = "pb"; //七牛公开空间
+//	mod.QNPRISPACE = "pv"; //七牛私有空间
+//	mod.XXTNOTICE = 'notice/'; //笔校讯通、通知
 
-	mod.BADGENUMBER = 'badgeNumber' //app角标
+//	mod.BADGENUMBER = 'badgeNumber' //app角标
 	mod.PERSONALINFO = 'personalInfo1111'; //个人信息，登录成功后返回值
 	mod.SHAKEHAND = 'ShakeHand'; //公钥，登录时，返回的握手信息，
 	mod.AUTOLOGIN = 'autoLogin'; //登录信息
-	mod.DOCUMENTSPATH = 'DOCUMENTSPATH'; //记录document的地址
 
 	mod.PUBLICPARAMETER = 'publicParameter' //共用参数
-	mod.NEWDETAIL = 'newDetail' //新建通知、事务，记录页面填的数据
 
 	mod.WAITING = '加载中...'; //
 	mod.UPLOADING = '上传中...';
 	mod.SIGNKEY = 'jsy309'; //签名密钥
 	
-	mod.ISFIRST = 'isFitst'; //是否是第一次登陆
-	mod.ICONNUM = 0; //角标数量
-	
-	//口语测评host：https://res.jiaobaowang.net; http://139.129.252.49:8080/res; http://192.168.0.122:801/res; http://139.129.252.49:8080/speeking
-	mod.ORALSHOST = "http://139.129.252.49:8080/speeking";
+//	mod.ISFIRST = 'isFitst'; //是否是第一次登陆
+//	mod.ICONNUM = 0; //角标数量
 	
 	/**
 	 * 在本地存永久数据
